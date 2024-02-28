fd0f1f61d76961fe80cc0176640224df84213006
Md Adnan Abir <100922574+Md-Adnan-Abir@users.noreply.github.com>
2024-02-28 02:34:50 +0100

    solve first 2 problem

---
+++ Java/01_Introduction/Stdin_and_Stdout_I.java
@@ -0,0 +1,22 @@
+
+/**
+ * In this challenge, you must read  integers from stdin and then print them to stdout.
+ * Each integer must be printed on a new line.
+ * 
+ * @author (Md Adnan Abir) 
+ * @version (27022024)
+ */
+import java.util.*;
+
+public class Stdin_and_Stdout_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ Java/01_Introduction/Welcome_to_Java.java
@@ -0,0 +1,13 @@
+
+/**
+ * Welcome to the world of Java! In this challenge, we practice printing to stdout.
+ * 
+ * @author (Md Adnan Abir) 
+ * @version (26022024)
+ */
+public class Welcome_to_Java {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
